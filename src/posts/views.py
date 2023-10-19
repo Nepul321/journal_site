afb182473a148eca8c82d9d2eef18dce181e3e86
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-10-18 20:13:36 -0400

    Add items to the articles page

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 def AllArticles(request, *args, **kwargs):
     template = "global.html"
-    context = {
+    qs = Post.objects.all()
 
+    context = {
+      'qs' : qs,
     }
 
     return render(request, template, context)
