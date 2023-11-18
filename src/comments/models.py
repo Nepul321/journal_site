6ab4af06097ca7b4a6acbf321c7623611b0dc7fb
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-11-18 14:58:22 -0500

    Create a comments model

---
+++ src/comments/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300)
+    date = models.DateField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey("self", blank=True, null=True, on_delete=models.CASCADE)
+
+    # class Meta:
+    #     ordering = ['-datetime', ]
+
+    @property
+    def is_child(self):
+        if self.parent is not None:
+            return True
+        else:
+            return False
+
+    def get_children(self):
+        if self.is_child:
+            return None
+        else:
+            return Comment.objects.filter(parent=self)
