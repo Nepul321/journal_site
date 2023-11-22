b845c1d7d5ab5af3c10cb0e0a59566ac0f75325b
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-11-22 14:01:47 -0500

    Show comments in the frontend

--- src/users/models.py
+++ src/users/models.py
@@ -8,6 +8,7 @@ class CustomUser(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     is_author = models.BooleanField(default=False)
     name = models.CharField(max_length=255, blank=True)
+    profile_pic = models.ImageField(blank=True, upload_to="media/profile_pics/%Y/%m/%d/%H/%M")
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('username', )
     objects = CustomUserManager()
