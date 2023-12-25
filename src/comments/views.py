9192737653644163a5e2bfc9a75b64eaf0db2f9f
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-12-24 20:09:35 -0500

    Allow users to go back to the original article from the replies page

--- src/comments/views.py
+++ src/comments/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Comment
+from posts.models import Post
 
 # Create your views here.
 
@@ -7,9 +8,14 @@ def CommentReplies(request, id_, *args, **kwargs):
 	template = "comment_replies.html"
 	qs = Comment.objects.filter(id=id_)
 	obj = qs.first()
-
+	qs_2 = Post.objects.all()
+	answer = None
+	for i in qs_2:
+		if obj in i.comments.all():
+			answer = i
 	context = {
      'comment' : obj,
+     'post' : answer,
 	}
 
 	return render(request, template, context)
